ad5a53f53d58d247b1cac03fc12e86c3b8b08914
avinashchauhan914 <avinashsinghrajput914@gmail.com>
2021-08-25 14:38:20 +0530

    component created in admin

--- src/app/Admin/admin-routing.module.ts
+++ src/app/Admin/admin-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataGeneratorComponent } from '../Admin/data-generator/data-generator.component';
 
-const routes: Routes = [{}];
+const routes: Routes = [
+  {
+    path: 'datagenerator',
+    component: DataGeneratorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
